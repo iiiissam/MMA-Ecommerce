6b76e854e55ee9c7a9d96e8dd1aa28aea485f5df
hakimyl11 <hakimyaalaoui14@gmail.com>
2025-12-01 21:52:10 +0100

    Delete frontend/test.js

--- frontend/test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Home from "../src/app/page";
-
-test("La page affiche un texte", () => {
-  render(<Home />);
-  expect(screen.getByText("Bienvenue")).toBeInTheDocument();
-});
