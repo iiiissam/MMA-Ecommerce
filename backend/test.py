2931e6ba5fd271bcf2451a60feaa3beea3de6706
hakimyl11 <hakimyaalaoui14@gmail.com>
2025-12-01 21:50:01 +0100

    Delete backend/test.py

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-class SimpleTest(TestCase):
-    def test_homepage(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
-
