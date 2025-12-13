178410bb0b79092e602824697ba1e1e35b37c2f9
iiiissam <laibmohamed022020@gmail.com>
2025-12-12 16:16:45 -0800

    Fix: isot again

--- backend/store/management/commands/seed_women_fashion.py
+++ backend/store/management/commands/seed_women_fashion.py
@@ -2,14 +2,8 @@ import random
 
 from django.core.management.base import BaseCommand
 
-from store.models import (
-    Category,
-    Order,
-    OrderLine,
-    Product,
-    ProductImage,
-    ProductVariant,
-)
+from store.models import (Category, Order, OrderLine, Product, ProductImage,
+                          ProductVariant)
 
 
 class Command(BaseCommand):
